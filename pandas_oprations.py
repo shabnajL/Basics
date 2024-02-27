974d867e13625db0cb8e1421277670fb360d9812
Shabnaj L <shabnajliaqat@gmail.com>
2024-02-27 19:59:37 +0600

    Create pandas_oprations.py

---
+++ pandas_oprations.py
@@ -0,0 +1,17 @@
+#### 15 Python Pandas operations
+1. df.info() # Prints information about the DataFrame
+2. df.describe() # Generates state of the columns 
+3. df.head() # Prints first (n) rows of the DataFrame
+4. df.apply() # Applies functions on columns 
+5. df.groupby() # Applies aggregation of a column 
+6. df.sort_values() # Sorts tables 
+7. df.sample() # Sample data 
+8. pd.read_filetype(filename) # Imports from a filetype 
+9. df.to_filetype(filename) # Exports to a filetype
+10. df.plot() # Generates plot
+11. pd.to_datetime # Converts object to a datetime column
+11. df.filter() # Filter on columns
+12. df.drop() # Drops rows based on a condition 
+13. df.rank() # Generates ranks on a column
+14. df1.append(df2) # Adds the rows in df1 to the df2
+15. pd.isnull() # Checks the null values 
